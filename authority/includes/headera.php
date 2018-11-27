1331279311dd92985b9c85d5ccdd8120e8fcbcc7
indran <indajithc@hotmail.com>
2018-11-27 08:53:56 +0530

    some changes style

--- authority/includes/headera.php
+++ authority/includes/headera.php
@@ -105,14 +105,13 @@ $message = array();
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Dashboard</a>
+                <a class="navbar-brand" href="#"> </a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-left">
                     <li>
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="fa fa-dashboard"></i>
-                            <p class="hidden-lg hidden-md">Dashboard</p>
+
                         </a>
                     </li>
                     <li class="dropdown">
@@ -120,56 +119,48 @@ $message = array();
                         <i class="fa fa-globe"></i>
                         <b class="caret hidden-lg hidden-md"></b>
                         <p class="hidden-lg hidden-md">
-                          5 Notifications
+
                           <b class="caret"></b>
                       </p>
                   </a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Notification 1</a></li>
-                    <li><a href="#">Notification 2</a></li>
-                    <li><a href="#">Notification 3</a></li>
-                    <li><a href="#">Notification 4</a></li>
-                    <li><a href="#">Another notification</a></li>
-                </ul>
-            </li>
-            <li>
+                  
+              </li>
+              <li>
                <a href="">
-                <i class="fa fa-search"></i>
-                <p class="hidden-lg hidden-md">Search</p>
-            </a>
-        </li>
-    </ul>
 
-    <ul class="nav navbar-nav navbar-right">
+               </a>
+           </li>
+       </ul>
+
+       <ul class="nav navbar-nav navbar-right">
         <li>
            <a href="">
-               <p>Account</p>
+
            </a>
        </li>
        <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
             <p>
-              Dropdown
-              <b class="caret"></b>
-          </p>
+              
+            </p>
 
-      </a>
-      <ul class="dropdown-menu">
-        <li><a href="#">Action</a></li>
-        <li><a href="#">Another action</a></li>
-        <li><a href="#">Something</a></li>
-        <li><a href="#">Another action</a></li>
-        <li><a href="#">Something</a></li>
-        <li class="divider"></li>
-        <li><a href="#">Separated link</a></li>
-    </ul>
-</li>
-<li>
-    <a href="../loginn/logout.php">
-        <p>Log out</p>
-    </a>
-</li>
-<li class="separator hidden-lg"></li>
+        </a>
+        <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="../loginn/logout.php">
+            <p>Log out</p>
+        </a>
+    </li>
+    <li class="separator hidden-lg"></li>
 </ul>
 </div>
 </div>
